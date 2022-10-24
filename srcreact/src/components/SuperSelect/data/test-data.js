30735e3b84aa2f00c49eb1c9766cad06b09bc094
David Hall <david-2.hall@novartis.com>
2022-10-24 16:36:06 -0400

    Added test data

---
+++ srcreact/src/components/SuperSelect/data/test-data.js
@@ -0,0 +1,267 @@
+export const testItems = [
+  {
+    "groupLabel": "CARDIAC MARKERS",
+    "items": [
+      {
+        "label": "Creatine Kinase (U/L ), Serum or Plasma",
+        "value": "CKSI"
+      }
+    ]
+  },
+  {
+    "groupLabel": "COAGULATION",
+    "items": [
+      {
+        "label": "Activated Partial Thromboplastin Time (sec), Plasma",
+        "value": "APTTSI"
+      },
+      {
+        "label": "Prothrombin Intl. Normalized Ratio (RATIO), Plasma",
+        "value": "INRSI"
+      }
+    ]
+  },
+  {
+    "groupLabel": "DIFFERENTIAL",
+    "items": [
+      {
+        "label": "Basophils (10^9/L), Blood",
+        "value": "BASOSI"
+      },
+      {
+        "label": "Basophils/Leukocytes (%), Blood",
+        "value": "BASOLSI"
+      },
+      {
+        "label": "Eosinophils (10^9/L), Blood",
+        "value": "EOSSI"
+      },
+      {
+        "label": "Eosinophils/Leukocytes (%), Blood",
+        "value": "EOSLSI"
+      },
+      {
+        "label": "Lymphocytes (10^9/L), Blood",
+        "value": "LYMSI"
+      },
+      {
+        "label": "Lymphocytes/Leukocytes (%), Blood",
+        "value": "LYMLESI"
+      },
+      {
+        "label": "Monocytes (10^9/L), Blood",
+        "value": "MONOSI"
+      },
+      {
+        "label": "Monocytes/Leukocytes (%), Blood",
+        "value": "MONOLESI"
+      },
+      {
+        "label": "Neutrophils (10^9/L), Blood",
+        "value": "NEUTSI"
+      },
+      {
+        "label": "Neutrophils/Leukocytes (%), Blood",
+        "value": "NEUTLESI"
+      }
+    ]
+  },
+  {
+    "groupLabel": "ELECTROLYTES",
+    "items": [
+      {
+        "label": "Potassium (mmol/L), Serum or Plasma",
+        "value": "KSI"
+      },
+      {
+        "label": "Sodium (mmol/L), Serum or Plasma",
+        "value": "NASI"
+      }
+    ]
+  },
+  {
+    "groupLabel": "KIDNEY FUNCTION TESTS",
+    "items": [
+      {
+        "label": "Creatinine (umol/L), Serum or Plasma",
+        "value": "CREATSI"
+      },
+      {
+        "label": "Urea (mmol/L), Serum",
+        "value": "UREASI"
+      },
+      {
+        "label": "Urea Nitrogen (mmol/L), Blood",
+        "value": "BUNSI"
+      }
+    ]
+  },
+  {
+    "groupLabel": "LIPID PROFILE TESTS",
+    "items": [
+      {
+        "label": "Cholesterol (mmol/L), Serum",
+        "value": "CHOLSI"
+      },
+      {
+        "label": "HDL Cholesterol (mmol/L), Serum",
+        "value": "HDLSI"
+      },
+      {
+        "label": "LDL Cholesterol (mmol/L ), Serum",
+        "value": "LDLSI"
+      },
+      {
+        "label": "Triglycerides (mmol/L), Serum or Plasma",
+        "value": "TRIGSI"
+      }
+    ]
+  },
+  {
+    "groupLabel": "LIVER FUNCTION TESTS",
+    "items": [
+      {
+        "label": "Alanine Aminotransferase (U/L), Serum",
+        "value": "ALTSI"
+      },
+      {
+        "label": "Albumin (g/L), Serum",
+        "value": "ALBSI"
+      },
+      {
+        "label": "Alkaline Phosphatase (U/L), Serum",
+        "value": "ALPSI"
+      },
+      {
+        "label": "Aspartate Aminotransferase (U/L), Serum",
+        "value": "ASTSI"
+      },
+      {
+        "label": "Bilirubin (umol/L), Serum",
+        "value": "BILISI"
+      },
+      {
+        "label": "Direct Bilirubin (umol/L), Serum",
+        "value": "BILDRSI"
+      },
+      {
+        "label": "Gamma Glutamyl Transferase (U/L), Serum",
+        "value": "GGTSI"
+      },
+      {
+        "label": "Indirect Bilirubin (umol/L), Serum",
+        "value": "BILNDSI"
+      }
+    ]
+  },
+  {
+    "groupLabel": "OTHER",
+    "items": [
+      {
+        "label": "Amylase (U/L), Serum",
+        "value": "AMYLSI"
+      },
+      {
+        "label": "Amylase, Pancreatic",
+        "value": "AMYLASEP"
+      },
+      {
+        "label": "Bicarbonate (mmol/L ), Serum",
+        "value": "BICARBSI"
+      },
+      {
+        "label": "Calcium (mmol/L), Serum",
+        "value": "CASI"
+      },
+      {
+        "label": "Calcium Corrected (mmol/L ), Plasma/Serum",
+        "value": "CACORCSI"
+      },
+      {
+        "label": "Choriogonadotropin Beta (coded), Serum",
+        "value": "HCGCD"
+      },
+      {
+        "label": "Erythrocytes (10^12/L), Blood",
+        "value": "RBCSI"
+      },
+      {
+        "label": "Glucose (mmol/L ), Urine",
+        "value": "UGLUCSI"
+      },
+      {
+        "label": "Glucose (mmol/L), Plasma",
+        "value": "GLUCPSI"
+      },
+      {
+        "label": "Hematocrit (RATIO), Blood",
+        "value": "HCTSI"
+      },
+      {
+        "label": "Hemoglobin (g/L), Blood",
+        "value": "HGBSI"
+      },
+      {
+        "label": "Hemoglobin A1C (% ), Serum",
+        "value": "HBA1CSI"
+      },
+      {
+        "label": "Hemoglobin A1C (mmol/mol)",
+        "value": "HBA1C"
+      },
+      {
+        "label": "Insulin",
+        "value": "INSULIN"
+      },
+      {
+        "label": "Lactate Dehydrogenase (U/L), Serum",
+        "value": "LDHSI"
+      },
+      {
+        "label": "LDL Cholesterol",
+        "value": "LDL"
+      },
+      {
+        "label": "Leukocytes (10^9/L), Blood",
+        "value": "WBCSI"
+      },
+      {
+        "label": "Lipase, Pancreatic (U/L), Serum",
+        "value": "LIPPSI"
+      },
+      {
+        "label": "Magnesium (mmol/L), Serum",
+        "value": "MGSI"
+      },
+      {
+        "label": "Occult Blood, Urine, Dipstick",
+        "value": "OCCBLD"
+      },
+      {
+        "label": "Platelets (10^9/L), Blood",
+        "value": "PLATSI"
+      },
+      {
+        "label": "Protein (g/L), Serum",
+        "value": "TPROTSI"
+      },
+      {
+        "label": "Protein, Urine, Dipstick",
+        "value": "UPROTCD"
+      },
+      {
+        "label": "Urate (umol/L), Serum",
+        "value": "URATESI"
+      }
+    ]
+  },
+  {
+    "groupLabel": "PLANNED FASTING",
+    "items": [
+      {
+        "label": "Glucose (mmol/L), Plasma",
+        "value": "GLUCPSI"
+      }
+    ]
+  }
+]
